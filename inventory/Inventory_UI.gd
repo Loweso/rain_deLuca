428fa2b56f521ccbf90132b627be0acf5b61a66a
Loweso <dlouisefermin@gmail.com>
2024-05-23 16:34:37 +0800

    Court record finally working

--- inventory/Inventory_UI.gd
+++ inventory/Inventory_UI.gd
@@ -5,15 +5,33 @@ var is_Open = false
 
 @onready var inv: Inv = preload("res://inventory/inventory.tres")
 @onready var slots: Array = $Background/NinePatchRect/GridContainer.get_children()
+@onready var buttons: Array = $Background/NinePatchRect/ButtonContainer.get_children()
+
+@onready var item_display = $Background/Item_MainDisplay as Sprite2D
+@onready var item_name = $Background/Item_Name as Label
+@onready var item_desc = $Background/Item_Description as Label
 
 func _ready():
+	item_display.visible = false
+	item_name.visible = false
+	item_desc.visible = false
+	
+	for i in range(buttons.size()):
+		buttons[i].visible = false
+		buttons[i].pressed.connect(_update_item_display.bind(i))  # Connect using a lambda function
+	
 	close()
 	update_slots()
 	backButton.pressed.connect(toggle)
 
 func update_slots():
-	for i in range(min(inv.items.size(), slots.size())):
-		slots[i].update(inv.items[i])
+	for i in range(slots.size()):
+		if i < inv.items.size():
+			slots[i].update(inv.items[i])
+			buttons[i].visible = inv.items[i] != null
+		else:
+			slots[i].update(null)
+			buttons[i].visible = false
 
 func open():
 	self.visible = true
@@ -21,6 +39,9 @@ func open():
 
 func close():
 	self.visible = false
+	item_display.visible = false
+	item_name.visible = false
+	item_desc.visible = false
 	is_Open = false
 
 func toggle():
@@ -28,3 +49,13 @@ func toggle():
 		close()
 	else:
 		open()
+
+func _update_item_display(index: int):
+	item_display.visible = true
+	item_name.visible = true
+	item_desc.visible = true
+	
+	item_display.texture = inv.items[index].texture
+	item_display.scale = Vector2(0.003725, 0.007225)
+	item_name.text = inv.items[index].name
+	item_desc.text = inv.items[index].description
--- scenes/inv_ui_slot.gd
+++ scenes/inv_ui_slot.gd
@@ -1,21 +1,11 @@
 extends Panel
 
-@onready var isSelected = false
 @onready var item_visual = $item_display as Sprite2D
-@onready var item_button = $Button as Button
-
-func _ready():
-	item_button.pressed.connect(selectItem)
 
 func update(item: InvItem):
 	if !item:
 		item_visual.visible = false
-		item_button.visible = false
 	else:
 		item_visual.visible = true
-		item_button.visible = true
 		item_visual.texture = item.texture
 		item_visual.scale = Vector2(0.01725, 0.01725)
-		
-func selectItem():
-	isSelected = true
